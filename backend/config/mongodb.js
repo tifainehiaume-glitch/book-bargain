39bbf115ca3ac24cd462100fd3bc0467874cf6a9
Tifaine <tifaine.hiaume@gmail.com>
2026-06-26 20:46:13 +0200

    connection between the database of mysql.js  and mongodb.js

---
+++ backend/config/mongodb.js
@@ -0,0 +1,14 @@
+/* Connexion à la base de données MongoDB */
+const mongoose = require('mongoose');
+
+const connectMongo = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log('✅ MongoDB connecté');
+    } catch (error) {
+        console.error('❌ Erreur connexion MongoDB :', error.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectMongo;
